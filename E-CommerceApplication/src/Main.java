d856a695f959e95fb92449bd4388b340c597e378
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2023-12-26 09:48:58 +0100

    added main

---
+++ E-CommerceApplication/src/Main.java
@@ -0,0 +1,12 @@
+import services.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WelcomeScreen startingScreen=new WelcomeScreen();
+        startingScreen.displayWelcomeMessage();
+        startingScreen.landingScreen();
+
+    }
+}
